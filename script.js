66b6cbbbf1ad2453af97809ffe1d3e606f18c3ed
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-08-09 15:44:48 +0100

    Add local storage to books variable

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
 let books = [];
 const displayBooks = document.getElementById("displayBooks");
 let addButton = document.getElementById("add");
-
-
 function addBook () {
   let title = document.getElementById("title");
   let author = document.getElementById("author");
   let book = new formBook (title.value, author.value);
   books.push(book);
-
+  saveBooks();
   display ();
 }
-
 addButton.onclick = addBook;
-
-
-
 function removeBook (e) {
   let bookTitle = e.target.parentElement.childNodes[0].textContent;
   let bookAuthor = e.target.parentElement.childNodes[1].textContent;
   books = books.filter(book => book.title !== bookTitle || book.author !== bookAuthor);
   display();
 }
-
 function display () {
   displayBooks.innerHTML = " ";
   books.forEach(book => {
@@ -39,13 +32,20 @@ function display () {
     bookContainer.appendChild(bookAuthor);
     bookContainer.appendChild(remove);
   });
-  
 }
-
-
 function formBook (title, author){
   this.title = title;
   this.author = author;
 }
-
+function saveBooks() {
+  let myBooks = JSON.stringify(books);
+  localStorage.setItem('books', myBooks);
+}
+function checkStorage() {
+  if (localStorage.getItem('books')) {
+    books = JSON.parse(localStorage.getItem('books'));
+    display();
+  }
+}
+window.onload = checkStorage;
 display();
