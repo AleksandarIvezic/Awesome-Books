b0894baac47ca7cbc88481711294ba92d6f8a70a
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-08-09 14:19:18 +0200

    Create div for displaying all books

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let books = [];
+let books = ["book1", "book2"];
+const displayBooks = document.getElementById("displayBooks");
 
 function addBook (book) {
   books.push(book);
@@ -7,3 +8,15 @@ function addBook (book) {
 function removeBook (rmBook) {
   books = books.filter(book => book !== rmBook );
 }
+
+function display () {
+  books.forEach(book => {
+    let bookContainer = document.createElement("div");
+    bookContainer.style.borderBottom = "1px solid black";
+    bookContainer.innerText = book;
+    displayBooks.appendChild(bookContainer);
+  });
+  
+}
+
+display();
