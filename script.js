c921e9c23c74adb27dc932a2da6f01cc1e17aa8d
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-08-09 17:39:36 +0200

    Fix eslint errors

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 let books = [];
-const displayBooks = document.getElementById("displayBooks");
-let addButton = document.getElementById("add");
-function addBook () {
-  let title = document.getElementById("title");
-  let author = document.getElementById("author");
-  let book = new formBook (title.value, author.value);
-  books.push(book);
-  saveBooks();
-  display ();
+const displayBooks = document.getElementById('displayBooks');
+const addButton = document.getElementById('add');
+function FormBook(title, author) {
+  this.title = title;
+  this.author = author;
 }
-addButton.onclick = addBook;
-function removeBook (e) {
-  let bookTitle = e.target.parentElement.childNodes[0].textContent;
-  let bookAuthor = e.target.parentElement.childNodes[1].textContent;
-  books = books.filter(book => book.title !== bookTitle || book.author !== bookAuthor);
-  saveBooks();
-  display();
+function saveBooks() {
+  const myBooks = JSON.stringify(books);
+  localStorage.setItem('books', myBooks);
 }
-function display () {
-  displayBooks.innerHTML = " ";
-  books.forEach(book => {
-    let bookContainer = document.createElement("div");
-    let bookTitle = document.createElement("p");
-    let bookAuthor = document.createElement("p");
-    let remove = document.createElement("button");
-    remove.textContent = "Remove";
-    remove.onclick = removeBook;
+function display() {
+  displayBooks.innerHTML = ' ';
+  books.forEach((book) => {
+    const bookContainer = document.createElement('div');
+    const bookTitle = document.createElement('p');
+    const bookAuthor = document.createElement('p');
+    const remove = document.createElement('button');
+    remove.textContent = 'Remove';
+    remove.onclick = (e) => {
+      const bookTitle = e.target.parentElement.childNodes[0].textContent;
+      const bookAuthor = e.target.parentElement.childNodes[1].textContent;
+      books = books.filter((book) => book.title !== bookTitle || book.author !== bookAuthor);
+      saveBooks();
+      display();
+    };
     bookTitle.innerText = book.title;
     bookAuthor.innerText = book.author;
     displayBooks.appendChild(bookContainer);
@@ -34,14 +32,15 @@ function display () {
     bookContainer.appendChild(remove);
   });
 }
-function formBook (title, author){
-  this.title = title;
-  this.author = author;
+function addBook() {
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const book = new FormBook(title.value, author.value);
+  books.push(book);
+  saveBooks();
+  display();
 }
-function saveBooks() {
-  let myBooks = JSON.stringify(books);
-  localStorage.setItem('books', myBooks);
-}
+addButton.onclick = addBook;
 function checkStorage() {
   if (localStorage.getItem('books')) {
     books = JSON.parse(localStorage.getItem('books'));
